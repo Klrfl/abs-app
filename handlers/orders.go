a836193c56976319b63d8881551bcc9b0cfc5c3f
Efraim Munthe <efrayanglain@gmail.com>
2024-02-04 22:00:33 +0700

    update reading order handler

--- handlers/orders.go
+++ handlers/orders.go
@@ -17,7 +17,7 @@ func GetOrders(c *fiber.Ctx) error {
 	var id uuid.UUID
 	var err error
 
-	var orders []models.Order
+	var orders []*models.Order
 
 	var result *gorm.DB
 
@@ -31,9 +31,14 @@ func GetOrders(c *fiber.Ctx) error {
 			})
 		}
 
-		result = database.DB.Table("orders").Joins(joinQueryString).Select(selectQueryString).Where("members.id = ?", id).Find(&orders)
+		result = database.DB.
+			Preload("Member").
+			Where("members.id = ?", id).
+			Find(&orders)
 	} else {
-		result = database.DB.Table("orders").Joins(joinQueryString).Select(selectQueryString).Find(&orders)
+		result = database.DB.
+			Preload("Member").
+			Find(&orders)
 	}
 
 	if result.Error != nil {
@@ -49,6 +54,49 @@ func GetOrders(c *fiber.Ctx) error {
 			"message": "no orders yet",
 			"data":    orders,
 		})
+	}
+
+	// todo: make raw sql query to get []orderDetails
+
+	for _, order := range orders {
+		var orderDetails []*models.OrderDetail
+		rows, err := database.DB.Table("order_details").
+			Select("order_details.order_id, menu.id, menu.name, menu_types.type, menu_available_options.id, menu_available_options.option, menu_option_values.id, menu_option_values.value, order_details.quantity, order_details.quantity * variant_values.price as total_price").
+			Joins("join variant_values on order_details.menu_id=variant_values.menu_id and order_details.menu_option_value_id=variant_values.option_value_id").
+			Joins("join menu on order_details.menu_id=menu.id").
+			Joins("join menu_types on menu.type_id=menu_types.id").
+			Joins("join menu_option_values on order_details.menu_option_value_id=menu_option_values.id").
+			Joins("join menu_available_options on order_details.menu_option_id=menu_available_options.id").
+			Where("order_details.order_id = ?", order.ID).
+			Rows()
+
+		defer rows.Close()
+
+		if err != nil {
+			return c.JSON(fiber.Map{
+				"err":     true,
+				"message": "error when querying database for order_details",
+			})
+		}
+
+		for rows.Next() {
+			var orderDetail models.OrderDetail
+			rows.Scan(
+				&orderDetail.OrderID,
+				&orderDetail.MenuID,
+				&orderDetail.MenuName,
+				&orderDetail.MenuType,
+				&orderDetail.MenuOptionID,
+				&orderDetail.MenuOption,
+				&orderDetail.MenuOptionValueID,
+				&orderDetail.MenuOptionValue,
+				&orderDetail.Quantity,
+				&orderDetail.TotalPrice,
+			)
+			orderDetails = append(orderDetails, &orderDetail)
+		}
+
+		order.OrderDetails = orderDetails
 	}
 
 	return c.JSON(fiber.Map{
@@ -91,7 +139,7 @@ func GetOrderByID(c *fiber.Ctx) error {
 }
 
 func CreateNewOrder(c *fiber.Ctx) error {
-	newOrder := new(models.BaseOrder)
+	newOrder := new(models.Order)
 
 	if err := c.BodyParser(newOrder); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -125,10 +173,10 @@ func CompleteOrder(c *fiber.Ctx) error {
 		})
 	}
 
-	order := new(models.BaseOrder)
+	order := new(models.Order)
 
-	order.Id = id
-	order.Completed_at = time.Now()
+	order.ID = id
+	order.CompletedAt = time.Now()
 	result := database.DB.Model(&order).Updates(&order)
 
 	if result.Error != nil {
