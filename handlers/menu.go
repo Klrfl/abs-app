2eb8c31ff18046306d34b98d7a638277cc1dd057
Efraim Munthe <efrayanglain@gmail.com>
2024-03-24 20:28:49 +0700

    update menu type

--- handlers/menu.go
+++ handlers/menu.go
@@ -467,6 +467,54 @@ func DeleteMenuTypeByID(c *fiber.Ctx) error {
 
 	return c.JSON(fiber.Map{
 		"err":     false,
-		"message": fmt.Sprintf("successfully deleted menu type of ID", id),
+		"message": fmt.Sprintf("successfully deleted menu type of ID %d", id),
+	})
+}
+
+func UpdateMenuType(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"err":     true,
+			"message": "invalid ID",
+		})
+	}
+
+	var existingMenuType models.MenuType
+	var newMenuType models.MenuType
+
+	result := database.DB.
+		Where("id = ?", id).
+		Find(&existingMenuType).
+		Limit(1)
+
+	if result.Error != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"err":     true,
+			"message": "error when updating menu type",
+		})
+	}
+
+	if err := c.BodyParser(&newMenuType); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"err":     true,
+			"message": "something wrong with payload",
+		})
+	}
+
+	result = database.DB.
+		Where("id = ?", id).
+		Updates(&newMenuType)
+
+	if result.Error != nil {
+		return c.JSON(fiber.Map{
+			"err":     true,
+			"message": "something wrong when updating menu type",
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"err":     false,
+		"message": "successfully updated menu type",
 	})
 }
